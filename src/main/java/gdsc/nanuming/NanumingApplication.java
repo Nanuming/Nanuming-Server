375d73fe7193b6c60908f43aac0cdc5dc4c3d420
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-16 19:15:49 +0900

    chore: initial setup

---
+++ src/main/java/gdsc/nanuming/NanumingApplication.java
@@ -0,0 +1,13 @@
+package gdsc.nanuming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanumingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanumingApplication.class, args);
+	}
+
+}
