29ba921425c77f3a06c038877d2005eba7b95a75
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-24 02:22:44 +0900

    feat: add necessary fields on `ResponseDto.java`

--- src/main/java/gdsc/nanuming/common/ResponseDto.java
+++ src/main/java/gdsc/nanuming/common/ResponseDto.java
@@ -1,7 +1,14 @@
 package gdsc.nanuming.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 public class ResponseDto<T> {
 
+	private final boolean success;
+	private final int status;
+	private final String message;
 
+	@JsonInclude(JsonInclude.Include.NON_EMPTY)
+	private final T data;
 
 }
