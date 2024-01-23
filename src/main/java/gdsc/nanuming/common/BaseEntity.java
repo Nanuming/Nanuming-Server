f8198674d3a78fd5e1c35396afc026980dba2a72
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-24 01:47:39 +0900

    feat: add necessary annotations in `BaseEntity.java`

--- src/main/java/gdsc/nanuming/common/BaseEntity.java
+++ src/main/java/gdsc/nanuming/common/BaseEntity.java
@@ -1,4 +1,13 @@
 package gdsc.nanuming.common;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 }
