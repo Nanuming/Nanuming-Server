fc2cbca090333b448b4b0c647c2b39701963e345
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-24 01:39:03 +0900

    feat: add necessary fields and annotations in `Member.java`

--- src/main/java/gdsc/nanuming/member/entity/Member.java
+++ src/main/java/gdsc/nanuming/member/entity/Member.java
@@ -1,9 +1,42 @@
 package gdsc.nanuming.member.entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
+
+	@Id
+	@Column(name = "member_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String googleId;
+
+	private String name;
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime registrationDate;
+
+	@LastModifiedDate
+	private LocalDateTime updateDate;
+
+	private String profileUrl;
 }
