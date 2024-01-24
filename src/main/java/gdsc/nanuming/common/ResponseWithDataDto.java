df3b359497e622ccd1f6b7882b4ddbf31e70db0f
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-24 14:25:15 +0900

    feat: add necessary fields on `ResponseWithDataDto.java`

--- src/main/java/gdsc/nanuming/common/ResponseWithDataDto.java
+++ src/main/java/gdsc/nanuming/common/ResponseWithDataDto.java
@@ -1,4 +1,8 @@
 package gdsc.nanuming.common;
 
 public class ResponseWithDataDto {
+	private final boolean success;
+	private final int status;
+	private final String message;
+	private final T data;
 }
