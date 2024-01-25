50f085d766164d1dd9db5004275af9b6562c160e
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-25 11:51:02 +0900

    refactor: modify `ResponseWithDataDto.java` to `BaseResponseWithData.java`

--- src/main/java/gdsc/nanuming/common/response/BaseResponseWithData.java
+++ src/main/java/gdsc/nanuming/common/response/BaseResponseWithData.java
@@ -4,22 +4,22 @@ import gdsc.nanuming.common.code.Code;
 import lombok.Getter;
 
 @Getter
-public class ResponseWithDataDto<T> {
+public class BaseResponseWithData<T> {
 
 	private final boolean success;
 	private final int status;
 	private final String message;
 	private final T data;
 
-	private ResponseWithDataDto(Code code, T data) {
+	private BaseResponseWithData(Code code, T data) {
 		this.success = code.isSuccess();
 		this.status = code.getStatus();
 		this.message = code.getMessage();
 		this.data = data;
 	}
 
-	public static <T> ResponseWithDataDto<T> of(Code code, T data) {
-		return new ResponseWithDataDto<>(code, data);
+	public static <T> BaseResponseWithData<T> of(Code code, T data) {
+		return new BaseResponseWithData<>(code, data);
 	}
 
 }
