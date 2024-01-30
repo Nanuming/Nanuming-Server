27ab5e6567fc35936a3a9cf3c1828cffc37b3b20
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-30 12:49:25 +0900

    feat: add necessary annotations on `CustomOidcUserService.java`

--- src/main/java/gdsc/nanuming/oidc/service/CustomOidcUserService.java
+++ src/main/java/gdsc/nanuming/oidc/service/CustomOidcUserService.java
@@ -1,4 +1,12 @@
 package gdsc.nanuming.oidc.service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class CustomOidcUserService {
 }
