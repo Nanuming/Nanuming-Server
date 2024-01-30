9f21f28d0251f664aae9b5765d825d2e7fa68285
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-30 14:35:24 +0900

    feat: add inheritance on `CustomOidcUserService.java`

--- src/main/java/gdsc/nanuming/security/service/CustomOidcUserService.java
+++ src/main/java/gdsc/nanuming/security/service/CustomOidcUserService.java
@@ -1,5 +1,6 @@
 package gdsc.nanuming.security.service;
 
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class CustomOidcUserService {
+public class CustomOidcUserService extends OidcUserService {
+
 }
