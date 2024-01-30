9b450bbcf2996bf6124c41f0ddb2fe52b5b5f70f
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-30 12:40:02 +0900

    feat: add `@PrePersist`, `@PreUpdate` annotations and methods to save custom format LocalDateTime in DB

--- src/main/java/gdsc/nanuming/common/BaseEntity.java
+++ src/main/java/gdsc/nanuming/common/BaseEntity.java
@@ -1,12 +1,15 @@
 package gdsc.nanuming.common;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 
 @Getter
@@ -18,5 +21,21 @@ public abstract class BaseEntity {
 	private LocalDateTime createdAt;
 
 	private LocalDateTime updatedAt;
+
+	@PrePersist
+	public void onPrePersist() {
+		this.createdAt = formatting(LocalDateTime.now());
+		this.updatedAt = createdAt;
+	}
+
+	@PreUpdate
+	public void onPreUpdate() {
+		this.updatedAt = formatting(LocalDateTime.now());
+	}
+
+	private LocalDateTime formatting(LocalDateTime localDateTime) {
+		String customLocalDateTimeFormat = localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+		return LocalDateTime.parse(customLocalDateTimeFormat, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+	}
 
 }
