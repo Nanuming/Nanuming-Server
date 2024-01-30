41fbe28f761c8f4769bbb183e675203c5b8f6154
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-30 11:25:34 +0900

    feat: disable CSRF in `SecurityConfiguration.java`

--- src/main/java/gdsc/nanuming/config/SecurityConfiguration.java
+++ src/main/java/gdsc/nanuming/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
@@ -26,6 +27,8 @@ public class SecurityConfiguration {
 			return configuration;
 		}));
 
+		// disable CSRF
+		http.csrf(AbstractHttpConfigurer::disable);
 	}
 
 }
