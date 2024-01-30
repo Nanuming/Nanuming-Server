462af04727b6eec74e791aed8b2ee699906c1b97
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-30 16:30:26 +0900

    feat: add necessary constants

--- src/main/java/gdsc/nanuming/security/attributes/OidcAttributes.java
+++ src/main/java/gdsc/nanuming/security/attributes/OidcAttributes.java
@@ -6,10 +6,18 @@ import lombok.Getter;
 
 @Getter
 public class OidcAttributes {
+
 	private final Map<String, Object> attributes; // User attributes information
 	private final String attributeKey; // User attributes key
 	private final String providerId; // Provider + User unique number
 	private final String email; // User email
 	private final String provider; // IdP
+
+	private static final String GOOGLE = "google";
+
+	private static final String KEY = "key";
+	private static final String UUID = "providerId";
+	private static final String EMAIL = "email";
+	private static final String PROVIDER = "provider";
 
 }
