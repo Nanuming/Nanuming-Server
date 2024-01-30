e46b28fb6dcb34048ae0c1eb9fcfbee2ff1c4a53
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-30 12:30:01 +0900

    feat: add method to find member with email

--- src/main/java/gdsc/nanuming/member/repository/MemberRepository.java
+++ src/main/java/gdsc/nanuming/member/repository/MemberRepository.java
@@ -1,9 +1,13 @@
 package gdsc.nanuming.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import gdsc.nanuming.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByEmail(String email);
 
 }
