64b503cb08cd3a57b511f89da649c8ab859e579a
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-30 16:40:45 +0900

    feat: add `loadUser()` method in `CustomOidcUserService.java`

--- src/main/java/gdsc/nanuming/security/service/CustomOidcUserService.java
+++ src/main/java/gdsc/nanuming/security/service/CustomOidcUserService.java
@@ -1,9 +1,22 @@
 package gdsc.nanuming.security.service;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
 
+import gdsc.nanuming.member.MemberRole;
+import gdsc.nanuming.member.entity.Member;
 import gdsc.nanuming.member.repository.MemberRepository;
+import gdsc.nanuming.security.attributes.OidcAttributes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,4 +30,45 @@ public class CustomOidcUserService extends OidcUserService {
 
 	private final MemberRepository memberRepository;
 
+	@Override
+	public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+
+		OidcUser oidcUser = super.loadUser(userRequest);
+
+		String registrationId = userRequest.getClientRegistration().getRegistrationId();
+		log.info(">>> CustomOidcUserService loadUser() registrationId: {}", registrationId);
+		log.info(">>> registrationId now used as `provider`");
+
+		String userNameAttributeName = userRequest.getClientRegistration()
+			.getProviderDetails()
+			.getUserInfoEndpoint()
+			.getUserNameAttributeName();
+		log.info(">>> CustomOidcUserService loadUser() userNameAttributeName: {}", userNameAttributeName);
+
+		OidcAttributes oidcAttribute = OidcAttributes.of(registrationId, userNameAttributeName,
+			oidcUser.getAttributes());
+
+		Map<String, Object> memberAttribute = oidcAttribute.convertToMap();
+		log.info(">>> CustomOidcUserService loadUser() memberAttribute: {}", memberAttribute);
+
+		String email = (String)memberAttribute.get(EMAIL);
+
+		Optional<Member> findMember = memberRepository.findByEmail(email);
+
+		if (findMember.isEmpty()) {
+			memberAttribute.put(REGISTERED, false);
+			OidcUserInfo userInfo = new OidcUserInfo(memberAttribute);
+			return new DefaultOidcUser(
+				Collections.singleton(new SimpleGrantedAuthority(MemberRole.GUEST.getValue())),
+				oidcUser.getIdToken(), userInfo
+			);
+		}
+
+		memberAttribute.put(REGISTERED, true);
+		OidcUserInfo userInfo = new OidcUserInfo(memberAttribute);
+		return new DefaultOidcUser(
+			Collections.singleton(new SimpleGrantedAuthority(MemberRole.GUEST.getValue())),
+			oidcUser.getIdToken(), userInfo
+		);
+	}
 }
