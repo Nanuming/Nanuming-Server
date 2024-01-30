f150a22d52911ed207965d0c451882af130733e9
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-30 11:16:54 +0900

    feat: add necessary annotations on `SecurityConfiguration.java`

--- src/main/java/gdsc/nanuming/config/SecurityConfiguration.java
+++ src/main/java/gdsc/nanuming/config/SecurityConfiguration.java
@@ -1,4 +1,17 @@
 package gdsc.nanuming.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity(debug = true)
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+
+
 }
