a94358c345889ceffaf2ed90dd6a1286a6e0452b
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-30 17:10:19 +0900

    feat: add necessary annotations on `CustomAuthenticationSuccessHandler.java`

--- src/main/java/gdsc/nanuming/security/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/gdsc/nanuming/security/handler/CustomAuthenticationSuccessHandler.java
@@ -4,11 +4,17 @@ import java.io.IOException;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
