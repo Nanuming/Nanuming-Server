820e39ccf29ce735f939e41b43202978774c8fba
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-30 16:40:00 +0900

    feat: add `MemberRepository` variable in `CustomOidcUserService.java`

--- src/main/java/gdsc/nanuming/security/service/CustomOidcUserService.java
+++ src/main/java/gdsc/nanuming/security/service/CustomOidcUserService.java
@@ -3,6 +3,7 @@ package gdsc.nanuming.security.service;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.stereotype.Service;
 
+import gdsc.nanuming.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,4 +14,7 @@ public class CustomOidcUserService extends OidcUserService {
 
 	private static final String EMAIL = "email";
 	private static final String REGISTERED = "registered";
+
+	private final MemberRepository memberRepository;
+
 }
