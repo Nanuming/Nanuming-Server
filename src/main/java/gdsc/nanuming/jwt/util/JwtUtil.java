210bab39d00239ca69db41df404f9ac0b1995c7f
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-30 18:35:56 +0900

    feat: add `@Value` annotation to get value from Google Secret Manager

--- src/main/java/gdsc/nanuming/jwt/util/JwtUtil.java
+++ src/main/java/gdsc/nanuming/jwt/util/JwtUtil.java
@@ -2,6 +2,8 @@ package gdsc.nanuming.jwt.util;
 
 import org.springframework.stereotype.Service;
 
+import com.google.api.client.util.Value;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class JwtUtil {
 
+	@Value("${sm://JWT_SECRET}")
 	private String secret;
 	private String secretKey;
 
