b5721da4181e647be44e69ac77a803d3fab29dc2
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-30 16:26:56 +0900

    feat: add necessary fields in `OidcAttributes.java`

--- src/main/java/gdsc/nanuming/security/attributes/OidcAttributes.java
+++ src/main/java/gdsc/nanuming/security/attributes/OidcAttributes.java
@@ -1,7 +1,15 @@
 package gdsc.nanuming.security.attributes;
 
+import java.util.Map;
+
 import lombok.Getter;
 
 @Getter
 public class OidcAttributes {
+	private final Map<String, Object> attributes; // User attributes information
+	private final String attributeKey; // User attributes key
+	private final String providerId; // Provider + User unique number
+	private final String email; // User email
+	private final String provider; // IdP
+
 }
