4a784a4340430b80e580f6e656c8f8779cf7fbe1
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-01 02:16:13 +0900

    feat: add success code for token generation

--- src/main/java/gdsc/nanuming/common/code/CommonSuccessCode.java
+++ src/main/java/gdsc/nanuming/common/code/CommonSuccessCode.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 public enum CommonSuccessCode implements Code {
 
 	RESPONSE_SUCCESS(true, 200, "요청이 완료되었습니다."),
+	TOKEN_SUCCESS(true, 200, "토큰이 정상적으로 생성되었습니다.");
+
 	private final boolean success;
 	private final int status;
 	private final String message;
