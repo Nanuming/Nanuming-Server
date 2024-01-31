8c813323a83bd1b9593f7271af0230771851228e
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-31 10:57:27 +0900

    feat: add `memberRepository` in `MemberService.java`

--- src/main/java/gdsc/nanuming/member/service/MemberService.java
+++ src/main/java/gdsc/nanuming/member/service/MemberService.java
@@ -1,9 +1,14 @@
 package gdsc.nanuming.member.service;
 
 import org.springframework.stereotype.Service;
+
+import gdsc.nanuming.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+
+	private final MemberRepository memberRepository;
 
 }
