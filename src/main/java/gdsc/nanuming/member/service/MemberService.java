c657485940c617678977612dca59532569ba4056
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-31 10:56:46 +0900

    feat: add necessary annotations on `MemberService.java`

--- src/main/java/gdsc/nanuming/member/service/MemberService.java
+++ src/main/java/gdsc/nanuming/member/service/MemberService.java
@@ -1,5 +1,9 @@
 package gdsc.nanuming.member.service;
 
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+@Service
+@RequiredArgsConstructor
 public class MemberService {
 
 }
