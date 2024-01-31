a87fe074d5079f0048e7547e01a25d365e9fcb39
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-01 02:16:04 +0900

    feat: add success code for response

--- src/main/java/gdsc/nanuming/common/code/CommonSuccessCode.java
+++ src/main/java/gdsc/nanuming/common/code/CommonSuccessCode.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CommonSuccessCode implements Code {
+
+	RESPONSE_SUCCESS(true, 200, "요청이 완료되었습니다."),
 	private final boolean success;
 	private final int status;
 	private final String message;
