37693041a5c4622f878283050dc4e90c0920cff5
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-31 11:20:02 +0900

    feat: add `findByEmail()` method in `MemberService.java`

--- src/main/java/gdsc/nanuming/member/service/MemberService.java
+++ src/main/java/gdsc/nanuming/member/service/MemberService.java
@@ -1,8 +1,11 @@
 package gdsc.nanuming.member.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import gdsc.nanuming.member.entity.Member;
 import gdsc.nanuming.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -13,4 +16,7 @@ public class MemberService {
 
 	private final MemberRepository memberRepository;
 
+	public Optional<Member> findByEmail(String email) {
+		return memberRepository.findByEmail(email);
+	}
 }
