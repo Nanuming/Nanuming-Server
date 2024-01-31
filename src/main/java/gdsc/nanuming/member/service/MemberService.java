3ce72a7a96961a7012736f596c34d84770058b59
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-01-31 11:11:53 +0900

    feat: add `@Transactional` annotation on `MemberService.java`

--- src/main/java/gdsc/nanuming/member/service/MemberService.java
+++ src/main/java/gdsc/nanuming/member/service/MemberService.java
@@ -1,12 +1,14 @@
 package gdsc.nanuming.member.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import gdsc.nanuming.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
 
 	private final MemberRepository memberRepository;
