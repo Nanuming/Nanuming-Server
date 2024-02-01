cb38c3df89537ab0aae600590a89d709f92aaad3
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-01 22:59:28 +0900

    refactor: temporary comment processing

--- src/main/java/gdsc/nanuming/security/attributes/OidcAttributes.java
+++ src/main/java/gdsc/nanuming/security/attributes/OidcAttributes.java
@@ -1,68 +1,68 @@
-package gdsc.nanuming.security.attributes;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-public class OidcAttributes {
-
-	private final Map<String, Object> attributes; // User attributes information
-	private final String attributeKey; // User attributes key
-	private final String providerId; // Provider + User unique number
-	private final String email; // User email
-	private final String provider; // IdP
-
-	private static final String GOOGLE = "google";
-
-	private static final String KEY = "key";
-	private static final String UUID = "providerId";
-	private static final String EMAIL = "email";
-	private static final String PROVIDER = "provider";
-	private static final String UNDERSCORE = "_";
-
-	@Builder
-	private OidcAttributes(Map<String, Object> attributes, String attributeKey, String providerId, String email,
-		String provider) {
-		this.attributes = attributes;
-		this.attributeKey = attributeKey;
-		this.providerId = providerId;
-		this.email = email;
-		this.provider = provider;
-	}
-
-	public static OidcAttributes of(String provider, String attributeKey, Map<String, Object> attributes) {
-		switch (provider) {
-			case GOOGLE:
-				return ofGoogle(provider, attributeKey, attributes);
-			default:
-				throw new RuntimeException();
-				// TODO: create custom exception here
-		}
-	}
-
-	private static OidcAttributes ofGoogle(String provider, String attributeKey, Map<String, Object> attributes) {
-
-		// create UUID
-		String providerId = provider + UNDERSCORE + attributes.get(attributeKey);
-
-		return OidcAttributes.builder()
-			.providerId(providerId)
-			.email((String)attributes.get(EMAIL))
-			.provider(provider)
-			.attributes(attributes)
-			.attributeKey(attributeKey)
-			.build();
-	}
-
-	// return values as Map to put into OidcUser
-	public Map<String, Object> convertToMap() {
-		Map<String, Object> map = new HashMap<>();
-		map.put(UUID, providerId);
-		map.put(EMAIL, email);
-
-		return map;
-	}
-}
+// package gdsc.nanuming.security.attributes;
+//
+// import java.util.HashMap;
+// import java.util.Map;
+//
+// import lombok.Builder;
+// import lombok.Getter;
+//
+// @Getter
+// public class OidcAttributes {
+//
+// 	private final Map<String, Object> attributes; // User attributes information
+// 	private final String attributeKey; // User attributes key
+// 	private final String providerId; // Provider + User unique number
+// 	private final String email; // User email
+// 	private final String provider; // IdP
+//
+// 	private static final String GOOGLE = "google";
+//
+// 	private static final String KEY = "key";
+// 	private static final String UUID = "providerId";
+// 	private static final String EMAIL = "email";
+// 	private static final String PROVIDER = "provider";
+// 	private static final String UNDERSCORE = "_";
+//
+// 	@Builder
+// 	private OidcAttributes(Map<String, Object> attributes, String attributeKey, String providerId, String email,
+// 		String provider) {
+// 		this.attributes = attributes;
+// 		this.attributeKey = attributeKey;
+// 		this.providerId = providerId;
+// 		this.email = email;
+// 		this.provider = provider;
+// 	}
+//
+// 	public static OidcAttributes of(String provider, String attributeKey, Map<String, Object> attributes) {
+// 		switch (provider) {
+// 			case GOOGLE:
+// 				return ofGoogle(provider, attributeKey, attributes);
+// 			default:
+// 				throw new RuntimeException();
+// 				// TODO: create custom exception here
+// 		}
+// 	}
+//
+// 	private static OidcAttributes ofGoogle(String provider, String attributeKey, Map<String, Object> attributes) {
+//
+// 		// create UUID
+// 		String providerId = provider + UNDERSCORE + attributes.get(attributeKey);
+//
+// 		return OidcAttributes.builder()
+// 			.providerId(providerId)
+// 			.email((String)attributes.get(EMAIL))
+// 			.provider(provider)
+// 			.attributes(attributes)
+// 			.attributeKey(attributeKey)
+// 			.build();
+// 	}
+//
+// 	// return values as Map to put into OidcUser
+// 	public Map<String, Object> convertToMap() {
+// 		Map<String, Object> map = new HashMap<>();
+// 		map.put(UUID, providerId);
+// 		map.put(EMAIL, email);
+//
+// 		return map;
+// 	}
+// }
--- src/main/java/gdsc/nanuming/security/service/CustomOidcUserService.java
+++ src/main/java/gdsc/nanuming/security/service/CustomOidcUserService.java
@@ -1,74 +1,74 @@
-package gdsc.nanuming.security.service;
-
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
-
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.stereotype.Service;
-
-import gdsc.nanuming.member.MemberRole;
-import gdsc.nanuming.member.entity.Member;
-import gdsc.nanuming.member.service.MemberService;
-import gdsc.nanuming.security.attributes.OidcAttributes;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class CustomOidcUserService extends OidcUserService {
-
-	private static final String EMAIL = "email";
-	private static final String REGISTERED = "registered";
-
-	private final MemberService memberService;
-
-	@Override
-	public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
-
-		OidcUser oidcUser = super.loadUser(userRequest);
-
-		String registrationId = userRequest.getClientRegistration().getRegistrationId();
-		log.info(">>> CustomOidcUserService loadUser() registrationId: {}", registrationId);
-		log.info(">>> registrationId now used as `provider`");
-
-		String userNameAttributeName = userRequest.getClientRegistration()
-			.getProviderDetails()
-			.getUserInfoEndpoint()
-			.getUserNameAttributeName();
-		log.info(">>> CustomOidcUserService loadUser() userNameAttributeName: {}", userNameAttributeName);
-
-		OidcAttributes oidcAttribute = OidcAttributes.of(registrationId, userNameAttributeName,
-			oidcUser.getAttributes());
-
-		Map<String, Object> memberAttribute = oidcAttribute.convertToMap();
-		log.info(">>> CustomOidcUserService loadUser() memberAttribute: {}", memberAttribute);
-
-		String email = (String)memberAttribute.get(EMAIL);
-
-		Optional<Member> findMember = memberService.findByEmail(email);
-
-		if (findMember.isEmpty()) {
-			memberAttribute.put(REGISTERED, false);
-			OidcUserInfo userInfo = new OidcUserInfo(memberAttribute);
-			return new DefaultOidcUser(
-				Collections.singleton(new SimpleGrantedAuthority(MemberRole.GUEST.getValue())),
-				oidcUser.getIdToken(), userInfo
-			);
-		}
-
-		memberAttribute.put(REGISTERED, true);
-		OidcUserInfo userInfo = new OidcUserInfo(memberAttribute);
-		return new DefaultOidcUser(
-			Collections.singleton(new SimpleGrantedAuthority(MemberRole.USER.getValue())),
-			oidcUser.getIdToken(), userInfo
-		);
-	}
-}
+// package gdsc.nanuming.security.service;
+//
+// import java.util.Collections;
+// import java.util.Map;
+// import java.util.Optional;
+//
+// import org.springframework.security.core.authority.SimpleGrantedAuthority;
+// import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+// import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+// import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+// import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+// import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+// import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+// import org.springframework.stereotype.Service;
+//
+// import gdsc.nanuming.member.MemberRole;
+// import gdsc.nanuming.member.entity.Member;
+// import gdsc.nanuming.member.service.MemberService;
+// import gdsc.nanuming.security.attributes.OidcAttributes;
+// import lombok.RequiredArgsConstructor;
+// import lombok.extern.slf4j.Slf4j;
+//
+// @Slf4j
+// @Service
+// @RequiredArgsConstructor
+// public class CustomOidcUserService extends OidcUserService {
+//
+// 	private static final String EMAIL = "email";
+// 	private static final String REGISTERED = "registered";
+//
+// 	private final MemberService memberService;
+//
+// 	@Override
+// 	public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+//
+// 		OidcUser oidcUser = super.loadUser(userRequest);
+//
+// 		String registrationId = userRequest.getClientRegistration().getRegistrationId();
+// 		log.info(">>> CustomOidcUserService loadUser() registrationId: {}", registrationId);
+// 		log.info(">>> registrationId now used as `provider`");
+//
+// 		String userNameAttributeName = userRequest.getClientRegistration()
+// 			.getProviderDetails()
+// 			.getUserInfoEndpoint()
+// 			.getUserNameAttributeName();
+// 		log.info(">>> CustomOidcUserService loadUser() userNameAttributeName: {}", userNameAttributeName);
+//
+// 		OidcAttributes oidcAttribute = OidcAttributes.of(registrationId, userNameAttributeName,
+// 			oidcUser.getAttributes());
+//
+// 		Map<String, Object> memberAttribute = oidcAttribute.convertToMap();
+// 		log.info(">>> CustomOidcUserService loadUser() memberAttribute: {}", memberAttribute);
+//
+// 		String email = (String)memberAttribute.get(EMAIL);
+//
+// 		Optional<Member> findMember = memberService.findByEmail(email);
+//
+// 		if (findMember.isEmpty()) {
+// 			memberAttribute.put(REGISTERED, false);
+// 			OidcUserInfo userInfo = new OidcUserInfo(memberAttribute);
+// 			return new DefaultOidcUser(
+// 				Collections.singleton(new SimpleGrantedAuthority(MemberRole.GUEST.getValue())),
+// 				oidcUser.getIdToken(), userInfo
+// 			);
+// 		}
+//
+// 		memberAttribute.put(REGISTERED, true);
+// 		OidcUserInfo userInfo = new OidcUserInfo(memberAttribute);
+// 		return new DefaultOidcUser(
+// 			Collections.singleton(new SimpleGrantedAuthority(MemberRole.USER.getValue())),
+// 			oidcUser.getIdToken(), userInfo
+// 		);
+// 	}
+// }
