adff1d86cb80256cf9c9e73b8899731bd34e67f5
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-01 12:14:36 +0900

    refactor: rename `CommonSuccessCode.java` to `CommonCode.java`

--- src/main/java/gdsc/nanuming/common/code/CommonCode.java
+++ src/main/java/gdsc/nanuming/common/code/CommonCode.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum CommonSuccessCode implements Code {
+public enum CommonCode implements Code {
 
 	RESPONSE_SUCCESS(true, 200, "요청이 완료되었습니다."),
 	TOKEN_SUCCESS(true, 200, "토큰이 정상적으로 생성되었습니다.");
