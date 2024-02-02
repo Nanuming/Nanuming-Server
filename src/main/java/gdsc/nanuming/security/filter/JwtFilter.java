d806d29d35fef9cd6fd58b486e9377a62443f56a
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-02 21:24:41 +0900

    feat: add necessary fields in `JwtFilter.java`

--- src/main/java/gdsc/nanuming/security/filter/JwtFilter.java
+++ src/main/java/gdsc/nanuming/security/filter/JwtFilter.java
@@ -1,8 +1,13 @@
 package gdsc.nanuming.security.filter;
 
+import gdsc.nanuming.security.jwt.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class JwtFilter {
 
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+	public static final String BEARER_PREFIX = "Bearer ";
+
+	private final JwtUtil jwtUtil;
 }
