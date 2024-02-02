5c524954969a4eca76a1f7e7b388ad4bf92aa7d6
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-02 22:17:26 +0900

    feat: add `JwtAccessDeniedHandler.java`

---
+++ src/main/java/gdsc/nanuming/security/handler/JwtAccessDeniedHandler.java
@@ -0,0 +1,23 @@
+package gdsc.nanuming.security.handler;
+
+import java.io.IOException;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+		AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+		// TODO: create custom ErrorResponse here
+		// when attempting to access without necessary permissions -> return 403
+	}
+}
