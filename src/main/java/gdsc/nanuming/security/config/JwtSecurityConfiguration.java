c206d5bbc29def0bd1c5954e1d9d27b733706234
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-02 21:45:43 +0900

    feat: modify `JwtSecurityConfiguration.java` to extend `SecurityConfigurerAdapter`

--- src/main/java/gdsc/nanuming/security/config/JwtSecurityConfiguration.java
+++ src/main/java/gdsc/nanuming/security/config/JwtSecurityConfiguration.java
@@ -1,8 +1,14 @@
 package gdsc.nanuming.security.config;
 
-public class JwtSecurityConfiguration {
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+public class JwtSecurityConfiguration extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+
 
 }
