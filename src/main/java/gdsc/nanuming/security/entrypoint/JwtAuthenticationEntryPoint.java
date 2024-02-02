97c379421cca1fb8bfd567141bd07077e0fccf2e
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-02 22:14:31 +0900

    feat: add `JwtAccessDeniedHandler.java`

--- src/main/java/gdsc/nanuming/security/entrypoint/JwtAuthenticationEntryPoint.java
+++ src/main/java/gdsc/nanuming/security/entrypoint/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,22 @@
 package gdsc.nanuming.security.entrypoint;
 
-public class JwtAuthenticationEntryPoint {
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException authException) throws IOException, ServletException {
+
+		// TODO: create custom ErrorResponse here
+		// when attempting to access without providing valid credentials -> return 401
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+	}
 }
