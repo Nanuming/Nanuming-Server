ff8899779c78320ab36168b0698257453f808f95
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-02 11:04:13 +0900

    feat: add `@Getter` annotation on `LoginResponse.java`

--- src/main/java/gdsc/nanuming/member/dto/LoginResponse.java
+++ src/main/java/gdsc/nanuming/member/dto/LoginResponse.java
@@ -1,7 +1,9 @@
 package gdsc.nanuming.member.dto;
 
 import gdsc.nanuming.security.jwt.dto.GeneratedToken;
+import lombok.Getter;
 
+@Getter
 public class LoginResponse {
 
 	private GeneratedToken token;
