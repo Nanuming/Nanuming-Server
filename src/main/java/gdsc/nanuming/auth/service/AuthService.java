353c2295318cd977ace22517368de92f108bf9c1
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-03 16:32:30 +0900

    feat: add necessary annotations on `AuthService.java`

--- src/main/java/gdsc/nanuming/auth/service/AuthService.java
+++ src/main/java/gdsc/nanuming/auth/service/AuthService.java
@@ -1,4 +1,10 @@
 package gdsc.nanuming.auth.service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
 }
