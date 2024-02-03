7417309a51544c4ee3a9aa72ba7e5fd1a034a383
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-03 16:34:12 +0900

    feat: add necessary fields in `AuthService.java`

--- src/main/java/gdsc/nanuming/auth/service/AuthService.java
+++ src/main/java/gdsc/nanuming/auth/service/AuthService.java
@@ -1,10 +1,20 @@
 package gdsc.nanuming.auth.service;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.stereotype.Service;
 
+import gdsc.nanuming.jwt.repository.RefreshTokenRepository;
+import gdsc.nanuming.jwt.util.JwtUtil;
+import gdsc.nanuming.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+
+	private final AuthenticationManagerBuilder authenticationManagerBuilder;
+	private final MemberRepository memberRepository;
+	private final JwtUtil jwtUtil;
+	private final RefreshTokenRepository refreshTokenRepository;
+
 }
