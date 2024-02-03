bf0e52a03ca2dcfdb01b8dd2b06d9374859ac4d4
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-03 18:42:04 +0900

    feat: override necessary fields in `CustomUserDetails.java`

--- src/main/java/gdsc/nanuming/security/util/CustomUserDetails.java
+++ src/main/java/gdsc/nanuming/security/util/CustomUserDetails.java
@@ -1,4 +1,47 @@
 package gdsc.nanuming.security.util;
 
-public class CustomUserDetails {
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetails implements UserDetails {
+
+
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
 }
