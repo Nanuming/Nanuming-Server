8cca62ae11d439e4dd178e40fd195aa3fa86b785
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-03 16:35:03 +0900

    feat: add `@Transactional` annotation on `AuthService.java`

--- src/main/java/gdsc/nanuming/auth/service/AuthService.java
+++ src/main/java/gdsc/nanuming/auth/service/AuthService.java
@@ -2,6 +2,7 @@ package gdsc.nanuming.auth.service;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import gdsc.nanuming.jwt.repository.RefreshTokenRepository;
 import gdsc.nanuming.jwt.util.JwtUtil;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AuthService {
 
 	private final AuthenticationManagerBuilder authenticationManagerBuilder;
