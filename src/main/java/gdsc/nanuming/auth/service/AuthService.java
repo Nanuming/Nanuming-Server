bc89ec6f7a544468064b4134d1cd0579621d89cc
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-05 00:39:58 +0900

    feat: add `reissue()` method in `AuthService.java` for reissuing access token and refresh token for token expired user

--- src/main/java/gdsc/nanuming/auth/service/AuthService.java
+++ src/main/java/gdsc/nanuming/auth/service/AuthService.java
@@ -8,8 +8,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import gdsc.nanuming.auth.dto.request.LoginRequest;
 import gdsc.nanuming.auth.dto.request.RegisterRequest;
+import gdsc.nanuming.auth.dto.request.ReissueRequest;
 import gdsc.nanuming.auth.dto.response.LoginResponse;
 import gdsc.nanuming.auth.dto.response.RegisterResponse;
+import gdsc.nanuming.auth.dto.response.ReissueResponse;
 import gdsc.nanuming.jwt.config.JwtConfig;
 import gdsc.nanuming.jwt.dto.JwtToken;
 import gdsc.nanuming.jwt.entity.RefreshToken;
@@ -40,7 +42,8 @@ public class AuthService {
 
 		if (memberRepository.existsByProviderId(providerId)) {
 			// TODO: create ErrorResponse here
-			throw new RuntimeException("이미 가입되어 있는 유저입니다.");
+			throw new RuntimeException("This user is already registered.");
+
 		}
 		Member member = memberRepository.save(registerRequest.toMember());
 
@@ -52,21 +55,20 @@ public class AuthService {
 
 		JwtToken jwtToken = jwtUtil.generateToken(authentication);
 
-		// TODO: need refactor - extract meethod RefreshToken building
+		// TODO: need refactor - extract method RefreshToken building
 		RefreshToken refreshToken = RefreshToken.of(
 			userDetails.getUsername(),
 			jwtToken.getRefreshToken(),
 			jwtConfig.getRefreshTokenPeriod());
-		log.info(">>> AuthService register() refreshToken: {}", refreshToken);
 
 		refreshTokenRepository.save(refreshToken);
 
-		// TODO: modify RegisterResponse like LoginResponse - using userDetails
-		return RegisterResponse.of(member, jwtToken);
+		return RegisterResponse.of(userDetails.getUsername(), userDetails.getNickname(), jwtToken);
 	}
 
 	@Transactional
 	public LoginResponse login(LoginRequest loginRequest) {
+
 		UsernamePasswordAuthenticationToken authenticationToken = loginRequest.toAuthentication();
 
 		Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
@@ -76,8 +78,8 @@ public class AuthService {
 		JwtToken jwtToken = jwtUtil.generateToken(authentication);
 
 		RefreshToken refreshToken = RefreshToken.of(
-			jwtToken.getRefreshToken(),
 			userDetails.getUsername(),
+			jwtToken.getRefreshToken(),
 			jwtConfig.getRefreshTokenPeriod());
 
 		refreshTokenRepository.save(refreshToken);
@@ -86,5 +88,33 @@ public class AuthService {
 	}
 
 	// TODO: refresh token reissuing method
+	@Transactional
+	public ReissueResponse reissue(ReissueRequest reissueRequest) {
 
+		String accessToken = reissueRequest.getAccessToken();
+		String refreshToken = reissueRequest.getRefreshToken();
+		String providerIdFromAccessToken = jwtUtil.getProviderId(accessToken);
+
+		// TODO: need custom exception processing - login required
+		if (!jwtUtil.validateToken(refreshToken)) {
+			throw new RuntimeException("Refresh Token is invalid.");
+		}
+
+		// TODO: need custom exception processing
+		RefreshToken storedRefreshToken = refreshTokenRepository.findById(providerIdFromAccessToken)
+			.orElseThrow(() -> new RuntimeException("The saved Refresh Token could not be found."));
+
+		if (!storedRefreshToken.getProviderId().equals(providerIdFromAccessToken)) {
+			throw new RuntimeException("User information from token does not match.");
+		}
+
+		Authentication authentication = jwtUtil.getAuthentication(accessToken);
+		CustomUserDetails userDetails = (CustomUserDetails)authentication.getPrincipal();
+		JwtToken jwtToken = jwtUtil.generateToken(authentication);
+
+		RefreshToken newRefreshToken = storedRefreshToken.updateRefreshToken(jwtToken.getRefreshToken());
+		refreshTokenRepository.save(newRefreshToken);
+
+		return ReissueResponse.of(userDetails.getUsername(), userDetails.getNickname(), jwtToken);
+	}
 }
