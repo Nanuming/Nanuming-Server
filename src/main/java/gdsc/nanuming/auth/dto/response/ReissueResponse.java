a06cb00f71f6446415604e78d28aaab2df4d92ad
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-05 00:36:19 +0900

    feat: add necessary fields in `ReissueResponse.java`

--- src/main/java/gdsc/nanuming/auth/dto/response/ReissueResponse.java
+++ src/main/java/gdsc/nanuming/auth/dto/response/ReissueResponse.java
@@ -1,4 +1,12 @@
 package gdsc.nanuming.auth.dto.response;
 
+import gdsc.nanuming.jwt.dto.JwtToken;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class ReissueResponse {
+	private String providerId;
+	private String nickname;
+	private JwtToken token;
 }
