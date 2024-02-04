b4e4ad9059a4044ee5e08fb63e801b0fc6a8ec5c
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-05 00:36:53 +0900

    feat: add private constructor with `@Builder` annotation in `ReissueResponse.java`

--- src/main/java/gdsc/nanuming/auth/dto/response/ReissueResponse.java
+++ src/main/java/gdsc/nanuming/auth/dto/response/ReissueResponse.java
@@ -6,7 +6,15 @@ import lombok.Getter;
 
 @Getter
 public class ReissueResponse {
+
 	private String providerId;
 	private String nickname;
 	private JwtToken token;
+
+	@Builder
+	private ReissueResponse(String providerId, String nickname, JwtToken token) {
+		this.providerId = providerId;
+		this.nickname = nickname;
+		this.token = token;
+	}
 }
