31950a82a5c6a8a078f9ea5b08addc84a3dbba5e
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-05 02:33:33 +0900

    feat: add `toAuthenticate()` method in `AuthenticationConvertible.java`

--- src/main/java/gdsc/nanuming/auth/dto/request/AuthenticationConvertible.java
+++ src/main/java/gdsc/nanuming/auth/dto/request/AuthenticationConvertible.java
@@ -1,4 +1,9 @@
 package gdsc.nanuming.auth.dto.request;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 public interface AuthenticationConvertible {
+
+	UsernamePasswordAuthenticationToken toAuthentication();
+
 }
