3199eaf4da9faad37209ef1b7ebcc68e4a3fe4e3
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-05 00:38:22 +0900

    refactor: modify fields in `RefreshToken.java`

--- src/main/java/gdsc/nanuming/jwt/entity/RefreshToken.java
+++ src/main/java/gdsc/nanuming/jwt/entity/RefreshToken.java
@@ -12,24 +12,24 @@ import lombok.Getter;
 public class RefreshToken {
 
 	@Id
-	private String refreshToken;
-
 	private String providerId;
+
+	private String refreshToken;
 
 	@TimeToLive
 	private Long tokenPeriod;
 
 	@Builder
-	private RefreshToken(final String refreshToken, final String providerId, final Long tokenPeriod) {
-		this.refreshToken = refreshToken;
+	private RefreshToken(final String providerId, final String refreshToken, final Long tokenPeriod) {
 		this.providerId = providerId;
+		this.refreshToken = refreshToken;
 		this.tokenPeriod = tokenPeriod;
 	}
 
-	public static RefreshToken of(String refreshToken, String providerId, Long tokenPeriod) {
+	public static RefreshToken of(String providerId, String refreshToken, Long tokenPeriod) {
 		return RefreshToken.builder()
-			.refreshToken(refreshToken)
 			.providerId(providerId)
+			.refreshToken(refreshToken)
 			.tokenPeriod(tokenPeriod)
 			.build();
 	}
