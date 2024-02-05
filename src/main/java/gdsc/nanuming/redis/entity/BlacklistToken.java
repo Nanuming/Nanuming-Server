9c70add27eadea84061b35fe7413f0f6410a6bdc
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-05 18:17:01 +0900

    feat: add necessary annotations and fields in `BlacklistToken.java`

--- src/main/java/gdsc/nanuming/redis/entity/BlacklistToken.java
+++ src/main/java/gdsc/nanuming/redis/entity/BlacklistToken.java
@@ -1,4 +1,27 @@
 package gdsc.nanuming.redis.entity;
 
+import org.springframework.data.annotation.Id;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class BlacklistToken {
+
+	@Id
+	private String accessToken;
+	private String status;
+
+	@Builder
+	private BlacklistToken(final String accessToken, final String status) {
+		this.accessToken = accessToken;
+		this.status = status;
+	}
+
+	public static BlacklistToken of(String accessToken, String status) {
+		return BlacklistToken.builder()
+			.accessToken(accessToken)
+			.status(status)
+			.build();
+	}
 }
