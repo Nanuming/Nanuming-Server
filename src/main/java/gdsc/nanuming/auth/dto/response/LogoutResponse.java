687d0bcd7eac73e4a2e83085a5e32e7ec8baf240
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-05 16:33:13 +0900

    feat: add necessary annotations and fields in `LogoutResponse.java`

--- src/main/java/gdsc/nanuming/auth/dto/response/LogoutResponse.java
+++ src/main/java/gdsc/nanuming/auth/dto/response/LogoutResponse.java
@@ -1,5 +1,21 @@
 package gdsc.nanuming.auth.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class LogoutResponse {
 
+	private String providerId;
+
+	@Builder
+	private LogoutResponse(String providerId) {
+		this.providerId = providerId;
+	}
+
+	public static LogoutResponse from(String providerId) {
+		return LogoutResponse.builder()
+			.providerId(providerId)
+			.build();
+	}
 }
