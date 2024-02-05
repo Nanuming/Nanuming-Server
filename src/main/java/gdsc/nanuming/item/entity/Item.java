a6350198f2f3e882b948b913cdaf48c7e68c27fd
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-05 21:52:06 +0900

    feat: add necessary fields in `Item.java`

--- src/main/java/gdsc/nanuming/item/entity/Item.java
+++ src/main/java/gdsc/nanuming/item/entity/Item.java
@@ -1,4 +1,17 @@
 package gdsc.nanuming.item.entity;
 
 public class Item {
+
+	private Long id;
+
+	private Member sharer;
+
+	private Long mainImageId;
+
+	private Long lockerId;
+
+	private String title;
+
+	private String description;
+
 }
