6f96f1d8560a6dcf549be08efe5d95c147959ff3
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-06 17:48:59 +0900

    feat: add static factory method in `ItemImage.java`

--- src/main/java/gdsc/nanuming/image/entity/ItemImage.java
+++ src/main/java/gdsc/nanuming/image/entity/ItemImage.java
@@ -1,6 +1,7 @@
 package gdsc.nanuming.image.entity;
 
 import gdsc.nanuming.common.BaseEntity;
+import gdsc.nanuming.item.entity.Item;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +13,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -35,4 +37,10 @@ public class ItemImage extends BaseEntity {
 		this.imageUrl = imageUrl;
 	}
 
+	public static ItemImage of(Item item, String imageUrl) {
+		return ItemImage.builder()
+			.item(item)
+			.imageUrl(imageUrl)
+			.build();
+	}
 }
