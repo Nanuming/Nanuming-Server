c48cffcbd3a53ab9ed743ee44ccf567113648266
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-06 17:47:20 +0900

    feat: add necessary annotations and fields in `Location.java`

--- src/main/java/gdsc/nanuming/location/entity/Location.java
+++ src/main/java/gdsc/nanuming/location/entity/Location.java
@@ -1,4 +1,31 @@
 package gdsc.nanuming.location.entity;
 
+import java.util.List;
+
+import gdsc.nanuming.locker.entity.Locker;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+
+@Getter
+@Entity
 public class Location {
+
+	@Id
+	@Column(name = "location_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String description;
+
+	private double latitude;
+
+	private double longitude;
+
+	@OneToMany(mappedBy = "location")
+	private List<Locker> lockerList;
 }
