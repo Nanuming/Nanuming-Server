8c92e7bae16224c5bc0d4f2fac2a8dc4819fd8db
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-06 14:27:48 +0900

    feat: modify `ItemRepository.java` to inherit `JpaRepository`

--- src/main/java/gdsc/nanuming/item/repository/ItemRepository.java
+++ src/main/java/gdsc/nanuming/item/repository/ItemRepository.java
@@ -1,4 +1,8 @@
 package gdsc.nanuming.item.repository;
 
-public interface ItemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import gdsc.nanuming.item.entity.Item;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
 }
