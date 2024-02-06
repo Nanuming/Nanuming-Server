d576319932a2662df2cd034ef70f79a3c698af15
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-06 17:47:34 +0900

    feat: add necessary annotations and fields in `Locker.java`

--- src/main/java/gdsc/nanuming/locker/entity/Locker.java
+++ src/main/java/gdsc/nanuming/locker/entity/Locker.java
@@ -1,4 +1,37 @@
 package gdsc.nanuming.locker.entity;
 
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import gdsc.nanuming.location.entity.Location;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Locker {
+
+	@Id
+	@Column(name = "locker_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "location_id")
+	private Location location;
+
+	@JdbcTypeCode(SqlTypes.VARCHAR)
+	private LockerSize size;
+
+	@JdbcTypeCode(SqlTypes.VARCHAR)
+	private LockerStatus status = LockerStatus.EMPTY;
 }
