163dcc83cbba0ad4fa06cb58898d52460cce3a49
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-07 20:39:58 +0900

    feat: add `addItem()` method in `ItemController.java`

--- src/main/java/gdsc/nanuming/item/controller/ItemController.java
+++ src/main/java/gdsc/nanuming/item/controller/ItemController.java
@@ -1,8 +1,15 @@
 package gdsc.nanuming.item.controller;
 
+import static gdsc.nanuming.common.code.CommonCode.*;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import gdsc.nanuming.common.response.BaseResponseWithData;
+import gdsc.nanuming.item.dto.request.AddItemRequest;
+import gdsc.nanuming.item.dto.response.AddItemResponse;
 import gdsc.nanuming.item.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,5 +21,11 @@ import lombok.extern.slf4j.Slf4j;
 public class ItemController {
 
 	private final ItemService itemService;
+
+	@PostMapping
+	public BaseResponseWithData<AddItemResponse> addItem(@RequestBody AddItemRequest addItemRequest) {
+		log.info(">>> Run ItemController addItem()");
+		return BaseResponseWithData.of(RESPONSE_SUCCESS, itemService.addItem(addItemRequest));
+	}
 
 }
