755accd64ad7b7b82ac96aa88720d49c8cce4a3c
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-07 20:39:32 +0900

    feat: add `ItemService.java`

--- src/main/java/gdsc/nanuming/item/controller/ItemController.java
+++ src/main/java/gdsc/nanuming/item/controller/ItemController.java
@@ -3,6 +3,7 @@ package gdsc.nanuming.item.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import gdsc.nanuming.item.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,5 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/item")
 @RequiredArgsConstructor
 public class ItemController {
+
+	private final ItemService itemService;
 
 }
