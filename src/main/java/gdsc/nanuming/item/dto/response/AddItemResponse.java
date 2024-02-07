6eab45786c8f8ad6b02af31b7d2a86c1c272f21b
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-07 17:15:26 +0900

    feat: add necessary annotations on `AddItemResponse.java`

--- src/main/java/gdsc/nanuming/item/dto/response/AddItemResponse.java
+++ src/main/java/gdsc/nanuming/item/dto/response/AddItemResponse.java
@@ -1,9 +1,14 @@
 package gdsc.nanuming.item.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class AddItemResponse {
 
 	private Long id;
 
+	@Builder
 	private AddItemResponse(Long id) {
 		this.id = id;
 	}
