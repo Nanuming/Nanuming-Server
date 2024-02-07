8f2bda30654b9be0ab4f39b12e246fe91746b07e
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-07 17:11:02 +0900

    feat: add necessary fields in `AddItemRequest.java`

--- src/main/java/gdsc/nanuming/item/dto/request/AddItemRequest.java
+++ src/main/java/gdsc/nanuming/item/dto/request/AddItemRequest.java
@@ -1,4 +1,13 @@
 package gdsc.nanuming.item.dto.request;
 
+import java.util.List;
+
 public class AddItemRequest {
+
+	private Long sharerId;
+	private Long lockerId;
+	private String title;
+	private String description;
+	private List<String> imageList;
+
 }
