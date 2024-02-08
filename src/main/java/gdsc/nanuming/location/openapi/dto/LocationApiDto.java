f3c5ba73dcc8b0437e0723d653f7a5639a4d3ffa
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-08 15:38:06 +0900

    feat: add necessary annotations

--- src/main/java/gdsc/nanuming/location/openapi/dto/LocationApiDto.java
+++ src/main/java/gdsc/nanuming/location/openapi/dto/LocationApiDto.java
@@ -1,5 +1,9 @@
 package gdsc.nanuming.location.openapi.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class LocationApiDto {
 
 	private long stCode;
@@ -9,6 +13,7 @@ public class LocationApiDto {
 	private double latitude;
 	private double longitude;
 
+	@Builder
 	private LocationApiDto(long stCode, long zipCode, String name, String address, double latitude, double longitude) {
 		this.stCode = stCode;
 		this.zipCode = zipCode;
