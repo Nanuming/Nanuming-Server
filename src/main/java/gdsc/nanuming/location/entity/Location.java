33cee81bb2aa24fcc2c221b81fb43281df5204fc
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-08 16:41:14 +0900

    feat: add `@NoArgsConstructor` annotation on `Location.java`

--- src/main/java/gdsc/nanuming/location/entity/Location.java
+++ src/main/java/gdsc/nanuming/location/entity/Location.java
@@ -9,11 +9,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Location {
 
 	@Id
