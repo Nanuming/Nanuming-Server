0eefbbd5ba35f57021333b3a7d5922ab48217ea5
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-08 15:37:19 +0900

    feat: add necessary fields in `LocationApiDto.java`

--- src/main/java/gdsc/nanuming/location/openapi/dto/LocationApiDto.java
+++ src/main/java/gdsc/nanuming/location/openapi/dto/LocationApiDto.java
@@ -1,4 +1,12 @@
 package gdsc.nanuming.location.openapi.dto;
 
 public class LocationApiDto {
+
+	private long stCode;
+	private long zipCode;
+	private String name;
+	private String address;
+	private double latitude;
+	private double longitude;
+
 }
