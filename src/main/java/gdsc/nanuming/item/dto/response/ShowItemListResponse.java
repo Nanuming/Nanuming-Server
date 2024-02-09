1ed38116756260bf0979f7ea23262db4932cbba8
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-09 13:09:03 +0900

    feat: add `ShowItemListResponse.java`

---
+++ src/main/java/gdsc/nanuming/item/dto/response/ShowItemListResponse.java
@@ -0,0 +1,13 @@
+package gdsc.nanuming.item.dto.response;
+
+import java.util.List;
+
+import gdsc.nanuming.item.dto.ItemOutlineDto;
+import lombok.Getter;
+
+@Getter
+public class ShowItemListResponse {
+
+	private List<ItemOutlineDto> itemOutlineDtoList;
+
+}
