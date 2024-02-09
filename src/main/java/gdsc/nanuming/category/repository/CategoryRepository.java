6e40db6de80f56e76a71c2421460e0997e1bea88
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-09 23:58:21 +0900

    refactor: remove unnecessary method and import statements in `CategoryRepository.java`

--- src/main/java/gdsc/nanuming/category/repository/CategoryRepository.java
+++ src/main/java/gdsc/nanuming/category/repository/CategoryRepository.java
@@ -1,13 +1,9 @@
 package gdsc.nanuming.category.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import gdsc.nanuming.category.entity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-	Optional<Category> findByCategoryName(String categoryName);
 
 }
