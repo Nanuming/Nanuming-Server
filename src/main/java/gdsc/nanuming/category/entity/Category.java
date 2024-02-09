080b0fa18df56733863adf10646cc877df1f8145
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-10 00:04:22 +0900

    feat: add private constructor and static factory method in `Category.java`

--- src/main/java/gdsc/nanuming/category/entity/Category.java
+++ src/main/java/gdsc/nanuming/category/entity/Category.java
@@ -9,9 +9,12 @@ import org.hibernate.type.SqlTypes;
 import gdsc.nanuming.item.entity.Item;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -22,6 +25,7 @@ public class Category {
 
 	@Id
 	@Column(name = "category_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@JdbcTypeCode(SqlTypes.VARCHAR)
@@ -30,4 +34,14 @@ public class Category {
 	@OneToMany(mappedBy = "category")
 	private List<Item> itemList = new ArrayList<>();
 
+	@Builder
+	private Category(CategoryName categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public static Category from(CategoryName categoryName) {
+		return Category.builder()
+			.categoryName(categoryName)
+			.build();
+	}
 }
