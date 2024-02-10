939ffde9b099022952db76d47ff06c49a09c012b
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-11 03:11:53 +0900

    feat: add new `memberId` field

--- src/main/java/gdsc/nanuming/auth/dto/response/RegisterResponse.java
+++ src/main/java/gdsc/nanuming/auth/dto/response/RegisterResponse.java
@@ -7,19 +7,22 @@ import lombok.Getter;
 @Getter
 public class RegisterResponse {
 
+	private Long memberId;
 	private String providerId;
 	private String nickname;
 	private JwtToken token;
 
 	@Builder
-	private RegisterResponse(String providerId, String nickname, JwtToken token) {
+	private RegisterResponse(Long memberId, String providerId, String nickname, JwtToken token) {
+		this.memberId = memberId;
 		this.providerId = providerId;
 		this.nickname = nickname;
 		this.token = token;
 	}
 
-	public static RegisterResponse of(String providerId, String nickname, JwtToken token) {
+	public static RegisterResponse of(Long memberId, String providerId, String nickname, JwtToken token) {
 		return RegisterResponse.builder()
+			.memberId(memberId)
 			.providerId(providerId)
 			.nickname(nickname)
 			.token(token)
