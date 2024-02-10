d49ed10b70a917ead1cd04ff75f84b60efb0db57
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-11 01:37:55 +0900

    refactor: modify fields and remove implementation

--- src/main/java/gdsc/nanuming/auth/dto/request/LoginRequest.java
+++ src/main/java/gdsc/nanuming/auth/dto/request/LoginRequest.java
@@ -1,16 +1,10 @@
 package gdsc.nanuming.auth.dto.request;
 
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-
 import lombok.Getter;
 
 @Getter
-public class LoginRequest implements AuthenticationConvertible {
+public class LoginRequest {
 
-	private String providerId;
+	private String idToken;
 
-	@Override
-	public UsernamePasswordAuthenticationToken toAuthentication() {
-		return new UsernamePasswordAuthenticationToken(this.providerId, null);
-	}
 }
