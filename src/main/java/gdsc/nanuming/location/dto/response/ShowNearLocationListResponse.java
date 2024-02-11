299558ec16fb341c1d3e824b02bccf37e032941e
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-11 23:32:17 +0900

    feat: add necessary field

--- src/main/java/gdsc/nanuming/location/dto/response/ShowNearLocationListResponse.java
+++ src/main/java/gdsc/nanuming/location/dto/response/ShowNearLocationListResponse.java
@@ -1,4 +1,11 @@
 package gdsc.nanuming.location.dto.response;
 
+import java.util.List;
+
+import gdsc.nanuming.location.dto.LocationWithItemCountDto;
+
 public class ShowNearLocationListResponse {
+
+	private List<LocationWithItemCountDto> locationWithItemCountDtoList;
+
 }
