2233cc1be4f702ab15745c7ed6345b67ed2b6a48
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-11 23:30:23 +0900

    feat: add necessary fields and annotation

--- src/main/java/gdsc/nanuming/location/dto/request/ShowNearLocationListRequestWithDistanceMeter.java
+++ src/main/java/gdsc/nanuming/location/dto/request/ShowNearLocationListRequestWithDistanceMeter.java
@@ -1,5 +1,11 @@
 package gdsc.nanuming.location.dto.request;
 
+import lombok.Getter;
+
+@Getter
 public class ShowNearLocationListRequestWithDistanceMeter {
 
+	private double latitude;
+	private double longitude;
+	private double radiusInKm;
 }
