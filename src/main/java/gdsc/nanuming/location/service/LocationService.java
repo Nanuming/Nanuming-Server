0590588b96f3a62409cb8366794f9f6c0e0c3e0e
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-11 14:04:58 +0900

    feat: add `saveLocationList()` method

--- src/main/java/gdsc/nanuming/location/service/LocationService.java
+++ src/main/java/gdsc/nanuming/location/service/LocationService.java
@@ -1,4 +1,23 @@
 package gdsc.nanuming.location.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import gdsc.nanuming.location.entity.Location;
+import gdsc.nanuming.location.repository.LocationRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class LocationService {
+
+	private final LocationRepository locationRepository;
+
+	@Transactional
+	public void saveLocationList(List<Location> locationList) {
+		locationRepository.saveAll(locationList);
+	}
 }
