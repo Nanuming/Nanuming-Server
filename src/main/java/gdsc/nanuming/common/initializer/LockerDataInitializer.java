b493b12470d8a4857be3330e98d81a28ad8962c9
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-12 16:52:28 +0900

    feat: add Locker initial data with `@EventListener` allocated method

--- src/main/java/gdsc/nanuming/common/initializer/LockerDataInitializer.java
+++ src/main/java/gdsc/nanuming/common/initializer/LockerDataInitializer.java
@@ -1,4 +1,47 @@
 package gdsc.nanuming.common.initializer;
 
+import static gdsc.nanuming.locker.entity.LockerSize.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import gdsc.nanuming.location.entity.Location;
+import gdsc.nanuming.location.openapi.event.OpenApiLoadedEvent;
+import gdsc.nanuming.location.repository.LocationRepository;
+import gdsc.nanuming.locker.entity.Locker;
+import gdsc.nanuming.locker.repository.LockerRepository;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class LockerDataInitializer {
+
+	private final LockerRepository lockerRepository;
+	private final LocationRepository locationRepository;
+	private static final int SMALL_COUNT = 6;
+	private static final int MIDDLE_COUNT = 4;
+	private static final int BIG_COUNT = 2;
+
+	@EventListener
+	private void lockerDataInsert(OpenApiLoadedEvent event) {
+		List<Locker> lockerList = new ArrayList<>();
+		long count = locationRepository.count();
+		for (long i = 1; i <= count; i++) {
+			Location location = locationRepository.findById(i).get();
+			for (int j = 0; j < SMALL_COUNT; j++) {
+				lockerList.add(Locker.of(location, SMALL));
+			}
+			for (int j = 0; j < MIDDLE_COUNT; j++) {
+				lockerList.add(Locker.of(location, MIDDLE));
+			}
+			for (int j = 0; j < BIG_COUNT; j++) {
+				lockerList.add(Locker.of(location, BIG));
+			}
+		}
+		lockerRepository.saveAll(lockerList);
+	}
+
 }
