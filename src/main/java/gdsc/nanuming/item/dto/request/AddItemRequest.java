f285d78c90d2a802ee8b5719b713164c37a5d745
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-14 13:11:36 +0900

    feat: add private constructor with `@Builder` annotation

--- src/main/java/gdsc/nanuming/item/dto/request/AddItemRequest.java
+++ src/main/java/gdsc/nanuming/item/dto/request/AddItemRequest.java
@@ -7,6 +7,8 @@ import org.springframework.web.multipart.MultipartFile;
 import gdsc.nanuming.category.entity.Category;
 import gdsc.nanuming.item.entity.Item;
 import gdsc.nanuming.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -17,6 +19,16 @@ public class AddItemRequest {
 	private String title;
 	private String description;
 	private List<MultipartFile> imageList;
+
+	@Builder
+	private AddItemRequest(Long sharerId, Long categoryId, String title, String description,
+		List<MultipartFile> imageList) {
+		this.sharerId = sharerId;
+		this.categoryId = categoryId;
+		this.title = title;
+		this.description = description;
+		this.imageList = imageList;
+	}
 
 	public Item toEntity(Member member, Category category) {
 		return Item.of(
