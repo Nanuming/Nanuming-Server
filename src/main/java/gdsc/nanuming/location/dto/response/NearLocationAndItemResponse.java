b777857fc6e1389042d0e48cc62b94e10ffc9cd3
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-15 03:22:52 +0900

    feat: add private constructor with `@Builder` annotation on and static factory method

--- src/main/java/gdsc/nanuming/location/dto/response/NearLocationAndItemResponse.java
+++ src/main/java/gdsc/nanuming/location/dto/response/NearLocationAndItemResponse.java
@@ -1,8 +1,24 @@
 package gdsc.nanuming.location.dto.response;
 
+import java.util.List;
+
+import gdsc.nanuming.location.dto.LocationWithItemOutlineListDto;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class NearLocationAndItemResponse {
 
+	List<LocationWithItemOutlineListDto> locationWithItemOutline;
 
+	@Builder
+	private NearLocationAndItemResponse(List<LocationWithItemOutlineListDto> locationWithItemOutline) {
+		this.locationWithItemOutline = locationWithItemOutline;
+	}
 
-
+	public static NearLocationAndItemResponse from(List<LocationWithItemOutlineListDto> locationWithItemOutline) {
+		return NearLocationAndItemResponse.builder()
+			.locationWithItemOutline(locationWithItemOutline)
+			.build();
+	}
 }
