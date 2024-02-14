7eef4834aa619c05cca5724ef0e110ecacec6162
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-15 03:15:25 +0900

    feat: add necessary fields and annotations

--- src/main/java/gdsc/nanuming/locker/service/LockerService.java
+++ src/main/java/gdsc/nanuming/locker/service/LockerService.java
@@ -1,4 +1,20 @@
 package gdsc.nanuming.locker.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import gdsc.nanuming.locker.entity.Locker;
+import gdsc.nanuming.locker.entity.LockerStatus;
+import gdsc.nanuming.locker.repository.LockerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class LockerService {
+
+	private final LockerRepository lockerRepository;
+
 }
