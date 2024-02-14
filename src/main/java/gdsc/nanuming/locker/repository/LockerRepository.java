9a20e1359f43ad25069f476f68be4dce448ce09b
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-15 03:14:35 +0900

    feat: add `findByLocationIdAndStatus()` method

--- src/main/java/gdsc/nanuming/locker/repository/LockerRepository.java
+++ src/main/java/gdsc/nanuming/locker/repository/LockerRepository.java
@@ -1,8 +1,14 @@
 package gdsc.nanuming.locker.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import gdsc.nanuming.locker.entity.Locker;
+import gdsc.nanuming.locker.entity.LockerStatus;
 
 public interface LockerRepository extends JpaRepository<Locker, Long> {
+
+	List<Locker> findByLocationIdAndStatus(Long locationId, LockerStatus status);
+
 }
