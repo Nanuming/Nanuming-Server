ae17eecac6837cb027a21fbba55cc9dd5dfbaff1
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-15 03:20:48 +0900

    refactor: modify `showNearLocationAndItem()` method

--- src/main/java/gdsc/nanuming/location/controller/LocationController.java
+++ src/main/java/gdsc/nanuming/location/controller/LocationController.java
@@ -1,15 +1,15 @@
 package gdsc.nanuming.location.controller;
 
+import static gdsc.nanuming.common.code.CommonCode.*;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import gdsc.nanuming.common.code.CommonCode;
 import gdsc.nanuming.common.response.BaseResponseWithData;
-import gdsc.nanuming.location.dto.request.ShowNearLocationListRequest;
-import gdsc.nanuming.location.dto.request.ShowNearLocationListRequestWithDistanceMeter;
-import gdsc.nanuming.location.dto.response.ShowNearLocationListResponse;
+import gdsc.nanuming.location.dto.request.NearLocationAndItemRequest;
+import gdsc.nanuming.location.dto.response.NearLocationAndItemResponse;
 import gdsc.nanuming.location.service.LocationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,12 +22,11 @@ public class LocationController {
 
 	private final LocationService locationService;
 
-	@PostMapping
-	public BaseResponseWithData<ShowNearLocationListResponse> showNearLocationList(
-		@RequestBody ShowNearLocationListRequest showNearLocationListRequest) {
-		log.info(">>> LocationController showNearLocationList()");
-		return BaseResponseWithData.of(CommonCode.RESPONSE_SUCCESS,
-			locationService.showNearLocationListAndItemCount(showNearLocationListRequest));
+	@PostMapping("/nearBy")
+	public BaseResponseWithData<NearLocationAndItemResponse> showNearLocationAndItem(
+		@RequestBody NearLocationAndItemRequest request) {
+		log.info(">>> Run LocationController showNearLocationAndItem()");
+		return BaseResponseWithData.of(RESPONSE_SUCCESS, locationService.getNearLocationAndItemList(request));
 	}
 
 }
