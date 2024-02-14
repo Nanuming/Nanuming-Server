5b3982356f02f1bb02a87a4adeabd8b1430edce8
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-14 15:14:49 +0900

    feat: add necessary fields

--- src/main/java/gdsc/nanuming/item/dto/request/ShowNearItemListRequest.java
+++ src/main/java/gdsc/nanuming/item/dto/request/ShowNearItemListRequest.java
@@ -1,4 +1,13 @@
 package gdsc.nanuming.item.dto.request;
 
+import lombok.Getter;
+
+@Getter
 public class ShowNearItemListRequest {
+
+	private double latitude;
+	private double longitude;
+	private double latitudeDelta;
+	private double longitudeDelta;
+
 }
