9aa5bf3a7d298ab2d19771389b17fc4b85c8a1cd
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-14 20:36:57 +0900

    feat: modify `ShowNearLocationListRequest.java` implement `UserMapPolygon.java`

--- src/main/java/gdsc/nanuming/location/dto/request/ShowNearLocationListRequest.java
+++ src/main/java/gdsc/nanuming/location/dto/request/ShowNearLocationListRequest.java
@@ -1,9 +1,10 @@
 package gdsc.nanuming.location.dto.request;
 
+import gdsc.nanuming.common.UserMapPolygon;
 import lombok.Getter;
 
 @Getter
-public class ShowNearLocationListRequest {
+public class ShowNearLocationListRequest implements UserMapPolygon {
 
 	private double latitude;
 	private double longitude;
