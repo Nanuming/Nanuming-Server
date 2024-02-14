97d49a1b34bd8f5f7eb2b83bfe8d73bec7f55957
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-14 20:35:51 +0900

    feat: add `UserMapPolygon.java`

--- src/main/java/gdsc/nanuming/common/UserMapPolygon.java
+++ src/main/java/gdsc/nanuming/common/UserMapPolygon.java
@@ -1,4 +1,13 @@
 package gdsc.nanuming.common;
 
 public interface UserMapPolygon {
+
+	double getLatitude();
+
+	double getLongitude();
+
+	double getLatitudeDelta();
+
+	double getLongitudeDelta();
+
 }
