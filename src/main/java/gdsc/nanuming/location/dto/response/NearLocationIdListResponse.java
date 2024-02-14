03ac3d5887b5e94dff41ebabaacaf379971c3d41
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-14 20:37:37 +0900

    feat: add necessary annotations,fields and private constructor with static factory method

--- src/main/java/gdsc/nanuming/location/dto/response/NearLocationIdListResponse.java
+++ src/main/java/gdsc/nanuming/location/dto/response/NearLocationIdListResponse.java
@@ -1,4 +1,23 @@
 package gdsc.nanuming.location.dto.response;
 
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class NearLocationIdListResponse {
+
+	private List<Long> locationIdList;
+
+	@Builder
+	private NearLocationIdListResponse(List<Long> locationIdList) {
+		this.locationIdList = locationIdList;
+	}
+
+	public static NearLocationIdListResponse from(List<Long> locationIdList) {
+		return NearLocationIdListResponse.builder()
+			.locationIdList(locationIdList)
+			.build();
+	}
 }
