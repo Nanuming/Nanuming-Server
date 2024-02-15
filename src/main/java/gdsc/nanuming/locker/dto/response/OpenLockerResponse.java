9126557e75e0783a4d5fba0060f20d364b9b1a7d
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-16 06:08:54 +0900

    feat: add fields and annotations

--- src/main/java/gdsc/nanuming/locker/dto/response/OpenLockerResponse.java
+++ src/main/java/gdsc/nanuming/locker/dto/response/OpenLockerResponse.java
@@ -1,4 +1,21 @@
 package gdsc.nanuming.locker.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class OpenLockerResponse {
+
+	private Long lockerId;
+
+	@Builder
+	private OpenLockerResponse(Long lockerId) {
+		this.lockerId = lockerId;
+	}
+
+	public static OpenLockerResponse from(Long lockerId) {
+		return OpenLockerResponse.builder()
+			.lockerId(lockerId)
+			.build();
+	}
 }
