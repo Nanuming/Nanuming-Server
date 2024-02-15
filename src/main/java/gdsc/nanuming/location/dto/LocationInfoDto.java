fa07fc7c6c5e0503c4fda9149661dd1f2ee2bd7b
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-15 23:32:53 +0900

    feat: add necessary annotations and fields

--- src/main/java/gdsc/nanuming/location/dto/LocationInfoDto.java
+++ src/main/java/gdsc/nanuming/location/dto/LocationInfoDto.java
@@ -1,5 +1,19 @@
 package gdsc.nanuming.location.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class LocationInfoDto {
 
+	private Long locationId;
+	private double latitude;
+	private double longitude;
+
+	@Builder
+	private LocationInfoDto(Long locationId, double latitude, double longitude) {
+		this.locationId = locationId;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
 }
