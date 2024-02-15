1a5e3734d645d5f86e15de76a56eadf3659f5f03
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-15 20:20:38 +0900

    feat: add necessary methods

--- src/main/java/gdsc/nanuming/redis/listener/RedisKeyExpiredListener.java
+++ src/main/java/gdsc/nanuming/redis/listener/RedisKeyExpiredListener.java
@@ -1,4 +1,34 @@
 package gdsc.nanuming.redis.listener;
 
-public class RedisKeyExpiredListener {
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.stereotype.Component;
+
+import gdsc.nanuming.reservation.service.ReservationService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class RedisKeyExpiredListener extends KeyExpirationEventMessageListener {
+
+	private final ReservationService reservationService;
+
+	public RedisKeyExpiredListener(RedisMessageListenerContainer listenerContainer,
+		ReservationService reservationService) {
+		super(listenerContainer);
+		this.reservationService = reservationService;
+	}
+
+	@Override
+	public void onMessage(Message message, byte[] pattern) {
+		String expiredKey = message.toString();
+		log.info(">>> expiredKey: {}", expiredKey);
+
+		String[] splitted = expiredKey.split(":");
+		Long memberId = Long.valueOf(splitted[0]);
+		Long lockerId = Long.valueOf(splitted[1]);
+
+		reservationService.changeStatusToExpired(memberId, lockerId);
+	}
 }
