ebcf58f3fb658947b6ed9796eb0d2f27faaa3e13
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-15 20:18:47 +0900

    feat: add necessary fields and annotations

--- src/main/java/gdsc/nanuming/reservation/dto/response/ReservationResponse.java
+++ src/main/java/gdsc/nanuming/reservation/dto/response/ReservationResponse.java
@@ -1,4 +1,19 @@
 package gdsc.nanuming.reservation.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class ReservationResponse {
+
+	private Long reservationId;
+	private Long memberId;
+	private Long lockerId;
+
+	@Builder
+	private ReservationResponse(Long reservationId, Long memberId, Long lockerId) {
+		this.reservationId = reservationId;
+		this.memberId = memberId;
+		this.lockerId = lockerId;
+	}
 }
