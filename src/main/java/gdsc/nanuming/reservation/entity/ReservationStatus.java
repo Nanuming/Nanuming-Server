4f844acc09c7d88f8cfa3c3cc847b0abd4372f3d
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-15 13:26:20 +0900

    feat: add necessary fields and annotations

--- src/main/java/gdsc/nanuming/reservation/entity/ReservationStatus.java
+++ src/main/java/gdsc/nanuming/reservation/entity/ReservationStatus.java
@@ -1,4 +1,15 @@
 package gdsc.nanuming.reservation.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ReservationStatus {
+
+	VALID("valid"),
+	EXPIRED("expired");
+
+	private final String status;
+
 }
