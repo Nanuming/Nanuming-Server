81e728f334d721eb1c2e94abdc9aaf60d0b7afbf
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-16 01:57:10 +0900

    feat: add necessary fields and annotations

--- src/main/java/gdsc/nanuming/item/dto/response/ConfirmImageResponse.java
+++ src/main/java/gdsc/nanuming/item/dto/response/ConfirmImageResponse.java
@@ -1,4 +1,21 @@
 package gdsc.nanuming.item.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class ConfirmImageResponse {
+
+	private Long confirmItemImageId;
+
+	@Builder
+	private ConfirmImageResponse(Long confirmItemImageId) {
+		this.confirmItemImageId = confirmItemImageId;
+	}
+
+	public static ConfirmImageResponse from(Long confirmItemImageId) {
+		return ConfirmImageResponse.builder()
+			.confirmItemImageId(confirmItemImageId)
+			.build();
+	}
 }
