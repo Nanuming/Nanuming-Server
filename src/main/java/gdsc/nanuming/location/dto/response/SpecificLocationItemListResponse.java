12e5fafc3853c60d151b1dffced737c700a6afc2
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-16 15:26:03 +0900

    feat: add necessary annotations and field

--- src/main/java/gdsc/nanuming/location/dto/response/SpecificLocationItemListResponse.java
+++ src/main/java/gdsc/nanuming/location/dto/response/SpecificLocationItemListResponse.java
@@ -1,4 +1,24 @@
 package gdsc.nanuming.location.dto.response;
 
+import java.util.List;
+
+import gdsc.nanuming.item.dto.ItemOutlineDto;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class SpecificLocationItemListResponse {
+
+	private List<ItemOutlineDto> itemOutlineDtoList;
+
+	@Builder
+	private SpecificLocationItemListResponse(List<ItemOutlineDto> itemOutlineDtoList) {
+		this.itemOutlineDtoList = itemOutlineDtoList;
+	}
+
+	public static SpecificLocationItemListResponse from(List<ItemOutlineDto> itemOutlineDtoList) {
+		return SpecificLocationItemListResponse.builder()
+			.itemOutlineDtoList(itemOutlineDtoList)
+			.build();
+	}
 }
