d5f512e0634f283eadef9947f01c1ec08d3b076c
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-17 03:02:19 +0900

    feat: add entity data initializers

--- src/main/java/gdsc/nanuming/common/initializer/ItemDataInitializer.java
+++ src/main/java/gdsc/nanuming/common/initializer/ItemDataInitializer.java
@@ -1,4 +1,38 @@
 package gdsc.nanuming.common.initializer;
 
+import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import gdsc.nanuming.common.initializer.bulk.ItemBulkRepository;
+import gdsc.nanuming.location.openapi.event.OpenApiLoadedEvent;
+import gdsc.nanuming.location.repository.LocationRepository;
+import gdsc.nanuming.locker.repository.LockerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class ItemDataInitializer {
+
+	private final ItemBulkRepository itemBulkRepository;
+	private final LocationRepository locationRepository;
+	private static final int BATCH_SIZE = 1000;
+
+	@Order(4)
+	@EventListener
+	@Transactional
+	public void itemDataInsert(OpenApiLoadedEvent event) {
+
+		log.info("itemDataInsert start");
+		long count = locationRepository.count();
+		for (long i = 1; i <= count; i += BATCH_SIZE) {
+			long end = Math.min(count, i + BATCH_SIZE);
+			itemBulkRepository.bulkInsertItem(i, end);
+		}
+		log.info("itemDataInsert finished");
+	}
+
 }
--- src/main/java/gdsc/nanuming/common/initializer/ItemImageDataInitializer.java
+++ src/main/java/gdsc/nanuming/common/initializer/ItemImageDataInitializer.java
@@ -1,4 +1,38 @@
 package gdsc.nanuming.common.initializer;
 
+import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import gdsc.nanuming.common.initializer.bulk.ItemImageBulkRepository;
+import gdsc.nanuming.item.repository.ItemRepository;
+import gdsc.nanuming.location.openapi.event.OpenApiLoadedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class ItemImageDataInitializer {
+
+	private final ItemRepository itemRepository;
+	private final ItemImageBulkRepository itemImageBulkRepository;
+
+	private static final int BATCH_SIZE = 1000;
+
+	@Order(5)
+	@EventListener
+	@Transactional
+	public void itemImageDataInsert(OpenApiLoadedEvent event) {
+
+		log.info("itemImageDataInsert start");
+		long itemCount = itemRepository.count();
+		for (long i = 1; i <= itemCount; i += BATCH_SIZE) {
+			long end = Math.min(itemCount, i + BATCH_SIZE);
+			itemImageBulkRepository.bulkInsertItemImage(i, end);
+		}
+		log.info("itemImageDataInsert end");
+	}
+
 }
