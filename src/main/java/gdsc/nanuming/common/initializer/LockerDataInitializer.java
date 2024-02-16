0a1433cd32fc35ce5cf5d35d3b8c83cbc4ceea2e
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-17 03:20:14 +0900

    feat: add `@Order` annotation for sequential operation

--- src/main/java/gdsc/nanuming/common/initializer/LockerDataInitializer.java
+++ src/main/java/gdsc/nanuming/common/initializer/LockerDataInitializer.java
@@ -1,12 +1,13 @@
 package gdsc.nanuming.common.initializer;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import gdsc.nanuming.common.initializer.bulk.LockerBulkRepository;
 import gdsc.nanuming.location.openapi.event.OpenApiLoadedEvent;
 import gdsc.nanuming.location.repository.LocationRepository;
-import gdsc.nanuming.locker.repository.LockerBulkRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,14 +21,18 @@ public class LockerDataInitializer {
 
 	private static final int BATCH_SIZE = 1000;
 
+	@Order(2)
 	@EventListener
 	@Transactional
 	public void lockerDataInsert(OpenApiLoadedEvent event) {
+
+		log.info("lockerDataInsert start");
 		long locationCount = locationRepository.count();
 		for (long i = 1; i <= locationCount; i += BATCH_SIZE) {
 			long end = Math.min(locationCount, i + BATCH_SIZE);
 			lockerBulkRepository.bulkInsertLocker(i, end);
 		}
+		log.info("lockerDataInsert finished");
 	}
 
 }
