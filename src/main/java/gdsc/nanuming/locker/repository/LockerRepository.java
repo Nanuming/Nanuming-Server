3426e56885216e4df7ea3c0a0d07b384b51e36ba
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-17 20:41:22 +0900

    refactor: add parameter on `findByLocationIdAndStatusAndItemItemStatus()` method

--- src/main/java/gdsc/nanuming/locker/repository/LockerRepository.java
+++ src/main/java/gdsc/nanuming/locker/repository/LockerRepository.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import gdsc.nanuming.item.entity.ItemStatus;
 import gdsc.nanuming.locker.entity.Locker;
 import gdsc.nanuming.locker.entity.LockerStatus;
 
 public interface LockerRepository extends JpaRepository<Locker, Long> {
 
-	List<Locker> findByLocationIdAndStatus(Long locationId, LockerStatus status);
+	List<Locker> findByLocationIdAndStatusAndItemItemStatus(Long locationId, LockerStatus status, ItemStatus itemStatus);
 
 }
