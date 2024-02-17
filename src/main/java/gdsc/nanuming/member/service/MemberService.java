3cb5e1e57af2e40f8506f7fae4010142eb2690d9
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-02-17 21:17:32 +0900

    feat: add `assignLocker()` method and `confirmImage()` method

--- src/main/java/gdsc/nanuming/member/service/MemberService.java
+++ src/main/java/gdsc/nanuming/member/service/MemberService.java
@@ -2,6 +2,7 @@ package gdsc.nanuming.member.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.security.core.Authentication;
@@ -9,12 +10,19 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import gdsc.nanuming.image.entity.ItemImage;
+import gdsc.nanuming.image.service.ImageService;
 import gdsc.nanuming.item.dto.ItemOutlineDto;
+import gdsc.nanuming.member.dto.request.AssignLockerRequest;
+import gdsc.nanuming.member.dto.request.ConfirmImageRequest;
+import gdsc.nanuming.member.dto.response.AssignLockerResponse;
+import gdsc.nanuming.member.dto.response.ConfirmImageResponse;
 import gdsc.nanuming.item.entity.Item;
 import gdsc.nanuming.item.entity.ItemStatus;
 import gdsc.nanuming.item.repository.ItemRepository;
+import gdsc.nanuming.locker.entity.Locker;
+import gdsc.nanuming.locker.repository.LockerRepository;
 import gdsc.nanuming.member.dto.response.ShowMemberItemList;
-import gdsc.nanuming.member.repository.MemberRepository;
 import gdsc.nanuming.security.util.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,8 +33,10 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional(readOnly = true)
 public class MemberService {
 
-	private final MemberRepository memberRepository;
+	private final LockerRepository lockerRepository;
 	private final ItemRepository itemRepository;
+
+	private final ImageService imageService;
 
 	private static final String NOT_ASSIGNED = "미보관";
 
@@ -59,6 +69,56 @@ public class MemberService {
 		}
 
 		return ShowMemberItemList.from(itemOutlineDtoList);
+	}
+
+	@Transactional
+	public AssignLockerResponse assignLocker(Long memberId, Long itemId, AssignLockerRequest request) {
+		log.info(">>> MemberService assignLocker()");
+
+		if (!Objects.equals(getCurrentUserDetails().getId(), memberId)) {
+			throw new IllegalArgumentException("Not permitted.");
+		}
+
+		Item item = itemRepository.findById(itemId)
+			.orElseThrow(() -> new IllegalArgumentException("No item found"));
+
+		Long sharerId = item.getSharer().getId();
+		if (!Objects.equals(sharerId, memberId) || !Objects.equals(sharerId, getCurrentUserDetails().getId())) {
+			throw new IllegalArgumentException("Not permitted.");
+		}
+
+		Locker locker = lockerRepository.findById(request.getLockerId())
+			.orElseThrow(() -> new IllegalArgumentException("No locker found."));
+
+		item.assignLocker(locker);
+
+		return AssignLockerResponse.of(item.getId(), locker.getId());
+	}
+
+	@Transactional
+	public ConfirmImageResponse confirmImage(Long memberId, Long itemId, ConfirmImageRequest request) {
+
+		if (!Objects.equals(getCurrentUserDetails().getId(), memberId)) {
+			throw new IllegalArgumentException("Not permitted.");
+		}
+
+		Item temporarySavedItem = itemRepository.findById(itemId)
+			.orElseThrow(() -> new IllegalArgumentException("No item found"));
+
+		Long sharerId = temporarySavedItem.getSharer().getId();
+		if (!Objects.equals(sharerId, memberId) || !Objects.equals(sharerId, getCurrentUserDetails().getId())) {
+			throw new IllegalArgumentException("Not permitted.");
+		}
+
+		log.info(">>> confirmImage: {}", request.getConfirmImage());
+		ItemImage confirmItemImage = imageService
+			.uploadConfirmItemImage(request.getConfirmImage(), temporarySavedItem);
+
+		confirmItemImage.setAsConfirmImage();
+		temporarySavedItem.getItemImageList().add(confirmItemImage);
+		temporarySavedItem.changeSaveStatusToAvailable();
+
+		return ConfirmImageResponse.from(confirmItemImage.getItemImageId());
 	}
 
 	private CustomUserDetails getCurrentUserDetails() {
