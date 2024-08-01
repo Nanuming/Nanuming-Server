97b590cc6d00ee7a6d9e3903c4f5e95166eb5023
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-08-01 23:47:07 +0900

    refactor: modify import statement

--- src/main/java/gdsc/nanuming/jwt/util/JwtUtil.java
+++ src/main/java/gdsc/nanuming/jwt/util/JwtUtil.java
@@ -6,7 +6,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
