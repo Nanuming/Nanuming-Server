84c4bd875feee94b9e1cb8cbed8bf8bb93101a78
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-08-01 15:36:15 +0900

    feat: add environment variables from parameter store

--- scripts/start_new_container.sh
+++ scripts/start_new_container.sh
@@ -6,6 +6,10 @@ IMAGE_REPOSITORY_URI=$(aws ssm get-parameter --name "/nanuming/IMAGE_REPOSITORY_
 DB_URL=$(aws ssm get-parameter --name "/nanuming/DB_URL" --with-decryption --query "Parameter.Value" --output text)
 DB_USER=$(aws ssm get-parameter --name "/nanuming/DB_USER" --with-decryption --query "Parameter.Value" --output text)
 DB_PASSWORD=$(aws ssm get-parameter --name "/nanuming/DB_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
+S3_BASE_URL=$(aws ssm get-parameter --name "/nanuming/S3_BASE_URL" --with decryption --query "Parameter.Value" --output text)
+MAIN_IMAGE_PATH=$(aws ssm get-parameter --name "/nanuming/MAIN_IMAGE_PATH" --with decryption --query "Parameter.Value" --output text)
+CONFIRM_IMAGE_PATH=$(aws ssm get-parameter --name "/nanuming/CONFIRM_IMAGE_PATH" --with decryption --query "Parameter.Value" --output text)
+
 
 
 aws ecr get-login-password --region "$AWS_REGION" | docker login --username aws --password-stdin "$DEFAULT_IMAGE_REPOSITORY_URI"
