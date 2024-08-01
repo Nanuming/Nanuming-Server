3399a5fda94a5c12b992ecf3740f5b5afffcaf92
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-08-01 12:54:47 +0900

    feat: add necessary import statements

--- src/main/java/gdsc/nanuming/common/config/AwsS3Config.java
+++ src/main/java/gdsc/nanuming/common/config/AwsS3Config.java
@@ -1,5 +1,12 @@
 package gdsc.nanuming.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
 @Configuration
 public class AwsS3Config {
 
