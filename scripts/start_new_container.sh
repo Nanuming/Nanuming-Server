bf2a77ce90ca111dad82dd5fb6259acc090fa5b7
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-08-02 21:51:30 +0900

    refactor: remove environment variables injection in `start_new_container.sh`

--- scripts/start_new_container.sh
+++ scripts/start_new_container.sh
@@ -1,34 +1,9 @@
 #!/bin/bash
 
-AWS_REGION=$(aws ssm get-parameter --name "/nanuming/AWS_REGION" --query "Parameter.Value" --output text)
-DEFAULT_IMAGE_REPOSITORY_URI=$(aws ssm get-parameter --name "/nanuming/DEFAULT_IMAGE_REPOSITORY_URI" --query "Parameter.Value" --output text)
-IMAGE_REPOSITORY_URI=$(aws ssm get-parameter --name "/nanuming/IMAGE_REPOSITORY_URI" --query "Parameter.Value" --output text)
-DB_URL=$(aws ssm get-parameter --name "/nanuming/DB_URL" --with-decryption --query "Parameter.Value" --output text)
-DB_USER=$(aws ssm get-parameter --name "/nanuming/DB_USER" --with-decryption --query "Parameter.Value" --output text)
-DB_PASSWORD=$(aws ssm get-parameter --name "/nanuming/DB_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
-S3_BASE_URL=$(aws ssm get-parameter --name "/nanuming/S3_BASE_URL" --with-decryption --query "Parameter.Value" --output text)
-MAIN_IMAGE_PATH=$(aws ssm get-parameter --name "/nanuming/MAIN_IMAGE_PATH" --with-decryption --query "Parameter.Value" --output text)
-CONFIRM_IMAGE_PATH=$(aws ssm get-parameter --name "/nanuming/CONFIRM_IMAGE_PATH" --with-decryption --query "Parameter.Value" --output text)
-IOS_CLIENT_ID=$(aws ssm get-parameter --name "/nanuming/IOS_CLIENT_ID" --with-decryption --query "Parameter.Value" --output text)
-JWT_ACCESS_TOKEN_PERIOD=$(aws ssm get-parameter --name "/nanuming/JWT_ACCESS_TOKEN_PERIOD" --with-decryption --query "Parameter.Value" --output text)
-JWT_REFRESH_TOKEN_PERIOD=$(aws ssm get-parameter --name "/nanuming/JWT_REFRESH_TOKEN_PERIOD" --with-decryption --query "Parameter.Value" --output text)
-
-
-
-
 aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$DEFAULT_IMAGE_REPOSITORY_URI"
 
 docker pull "$IMAGE_REPOSITORY_URI":latest
 
 docker run -d --name nanuming-server \
   -e SPRING_PROFILES_ACTIVE=prod \
-  -e DB_URL="$DB_URL" \
-  -e DB_USER="$DB_USER" \
-  -e DB_PASSWORD="$DB_PASSWORD" \
-  -e S3_BASE_URL="$S3_BASE_URL" \
-  -e MAIN_IMAGE_PATH="$MAIN_IMAGE_PATH" \
-  -e CONFIRM_IMAGE_PATH="$CONFIRM_IMAGE_PATH" \
-  -e IOS_CLIENT_ID="$IOS_CLIENT_ID" \
-  -e JWT_ACCESS_TOKEN_PERIOD="$JWT_ACCESS_TOKEN_PERIOD" \
-  -e JWT_REFRESH_TOKEN_PERIOD="$JWT_REFRESH_TOKEN_PERIOD" \
   "$IMAGE_REPOSITORY_URI":latest
