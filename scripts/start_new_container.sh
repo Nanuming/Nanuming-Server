258e7b96aec9224e8e8ddd8287c140a5de29dc4b
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-08-02 22:07:40 +0900

    feat: add environment variables injection

--- scripts/start_new_container.sh
+++ scripts/start_new_container.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+AWS_REGION=$(aws ssm get-parameter --name "/nanuming/AWS_REGION" --query "Parameter.Value" --output text)
+DEFAULT_IMAGE_REPOSITORY_URI=$(aws ssm get-parameter --name "/nanuming/DEFAULT_IMAGE_REPOSITORY_URI" --query "Parameter.Value" --output text)
+IMAGE_REPOSITORY_URI=$(aws ssm get-parameter --name "/nanuming/IMAGE_REPOSITORY_URI" --query "Parameter.Value" --output text)
+
 aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$DEFAULT_IMAGE_REPOSITORY_URI"
 
 docker pull "$IMAGE_REPOSITORY_URI":latest
